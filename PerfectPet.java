f7784428d53af473b7c7d392595343df79a34e2b
Broildler355 <fouadk2024@gmail.com>
2025-10-16 15:03:40 +0000

    Update 2

--- PerfectPet.java
+++ PerfectPet.java
@@ -1,4 +1,14 @@
 import java.util.*;
 public class PerfectPet {
-    
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("give me your favorite season now");
+        String season = input.nextLine();
+        System.out.println("give me your favorite color now");
+        String color = input.nextLine();
+        if (name == "blue") {
+
+        } 
+
+    }
 }
